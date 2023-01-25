fa084d85bd52b26e88a0733b66bca2bf1f02123b
Owais Raza <owaisrazasamad@gmail.com>
2023-01-26 02:55:06 +0500

    Added Assignment#1

---
+++ Assignment#1/app.js
@@ -0,0 +1,19 @@
+
+// ***CHAPTER 1 : ALERTS*** //
+
+
+// QUESTION#1
+// alert("Greetings, visitor.");
+
+// QUESTION#2
+// alert("Error! Please enter a valid password");
+
+// QUESTION#3
+// alert("Welcome to JS Land... \nHappy Coding!");
+
+// QUESTION4
+// alert("Welcome to JS Land...");
+// alert("Happy Coding!");
+
+// QUESTION#5
+// alert("Hello... I can use JS through my web browser's console");
