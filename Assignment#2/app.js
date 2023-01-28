2c9f5181953e8329e00edef2be17a8903b843892
Owais Raza <owaisrazasamad@gmail.com>
2023-01-28 18:29:57 +0500

    Added Assignment # 2 (Chp 2, 3 & 4)

---
+++ Assignment#2/app.js
@@ -0,0 +1,106 @@
+
+// ***CHAPTER 2 : VARIABLES FOR STRINGS*** //
+
+// QUESTION#1
+// var username;
+
+// QUESTION#2
+// var myName = "Muhammad Owais Raza";
+
+// QUESTION#3
+// var message;
+// message = "Hello World";
+// alert (message);
+
+// QUESTION#4
+// var studentName = "Jhone Doe";
+// var studentAge = "15 years old";
+// var studentAchievement = "Certified Mobile Application Development";
+// alert (studentName);
+// alert (studentAge);
+// alert (studentAchievement);
+
+// QUESTION#5
+// var pizza;
+// pizza = " PIZZA \n PIZZ \n PIZ \n PI \n P";
+// alert ( pizza );
+
+// QUESTION#6
+// var email = "owaisrazasamad@gmail.com";
+// alert ("My email address is " + email);
+
+// QUESTION#7
+// var book = "A smarter way to learn JavaScript";
+// alert ("I am trying to learn from the Book " + book);
+
+// QUESTION#8
+// document.write ("<h1> Yah! I can write HTML content through JavaScript </h1>");
+
+// QUESTION#9
+// var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert (design)
+
+// ***END*** //
+
+//-----------------------------------------------------------------------------------------//
+
+// ***CHAPTER 3 : VARIABLES FOR NUMBERS*** //
+
+// QUESTION#1
+// var age = "17 years old";
+// alert ("I am " + age);
+
+// QUESTION#2
+// var numberOfVisits = "1";
+// alert ("You have visited this site " + numberOfVisits + " times");
+
+// QUESTION#3
+// var birthYear = "2005";
+// document.write ("My birth year is " + birthYear + "<br> Data type of my declared variable is number");
+
+// QUESTION#4
+// var visitorName;
+// var productTitle;
+// var quantity;
+// visitorName = "John Doe";
+// productTitle = "T-shirt";
+// quantity = 5;
+// document.write (visitorName + " ordered " + quantity + productTitle + "(s) on XYZ Clothing store")
+
+// ***END*** //
+
+//-----------------------------------------------------------------------------------------//
+
+// ***CHAPTER 4 : VARIABLE NAMES: LEGAL & ILLEGAL*** //
+
+// QUESTION#1
+// var first, second, third;
+
+// QUESTION#2
+// var legal;
+// var LEGAL;
+// var legalVariable;
+// var legal_variable;
+// var legalVariable1;
+// var illegal variable;
+// var 1illegalVariable;
+// var "illegalVariable";
+// var alert;
+// var 1;
+
+// QUESTION#3 (a)
+// document.write ("<h1>Rules for naming JS variables</h1>");
+
+// QUESTION#3 (b)
+// document.write ("Variable names can only contain letters, numbers, dollar signs, and underscores. For example $my_1stVariable <br>");
+
+// QUESTION#3 (C)
+// document.write ("Variables must begin with a letter, dollar sign or underscore. For example $name, _name or name <br>");
+
+// QUESTION#3 (D)
+// document.write ("Variable names are case sensitive. <br>")
+
+// QUESTION#3 (E)
+// document.write ("Variable names should not be JS keywords.")
+
+// ***END*** //
