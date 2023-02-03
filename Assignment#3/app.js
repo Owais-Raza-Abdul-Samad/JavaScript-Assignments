fefc8963f05e47b1f2f839f64b55b76a48e92d8c
Owais Raza <owaisrazasamad@gmail.com>
2023-02-03 17:12:34 +0500

    Added Assignment # 3 (Chp 5,6,7,8 & 9)

---
+++ Assignment#3/app.js
@@ -0,0 +1,257 @@
+
+// ***CHAPTER 5 : MATH EXPRESSIONS*** //
+
+// QUESTION#1
+// var num1 = 3;
+// var num2 = 5;
+// var total = num1+num2;
+// document.write ("Sum of " + num1 + " and " + num2 + " is " + total);
+
+// QUESTION#2
+// var num1 = 3;
+// var num2 = 5;
+// var total = num1-num2;
+// document.write ("Subtraction of " + num1 + " and " + num2 + " is " + total + "<br>");
+// var total = num1*num2;
+// document.write ("Multiplication of " + num1 + " and " + num2 + " is " + total + "<br>");
+// var total = num1/num2;
+// document.write ("Division of " + num1 + " and " + num2 + " is " + total + "<br>");
+// var total = num1%num2;
+// document.write ("Modulus of " + num1 + " and " + num2 + " is " + total + "<br>");
+
+// QUESTION#3 (a)
+// var randomVariable;
+
+// QUESTION#3 (b)
+// document.write ("Value after variable declaration is : " + randomVariable + "<br>");
+
+// QUESTION#3 (c)
+// randomVariable = 5;
+
+// QUESTION#3 (d)
+// document.write ("Initial value : " + randomVariable + "<br>");
+
+// QUESTION#3 (e)
+// randomVariable++
+
+// QUESTION#3 (f)
+// document.write ("Value after increment is : " + randomVariable + "<br>");
+
+// QUESTION#3 (g)
+// randomVariable = randomVariable + 7;
+
+// QUESTION#3 (h)
+// document.write ("Value after addition is : " + randomVariable + "<br>");
+
+// QUESTION#3 (i)
+// randomVariable--
+
+// QUESTION#3 (j)
+// document.write ("Value after decrement is : " + randomVariable + "<br>");
+
+// QUESTION#3 (k)
+// randomVariable = randomVariable % 3
+
+// QUESTION#3 (l)
+// document.write ("The remainder is : " + randomVariable)
+
+// QUESTION#4
+// var ticketPrice = 600;
+// var  totalCost = ticketPrice * 5;
+// document.write ("Total cost to buy 5 tickets to amovie is " +  totalCost + " PKR")
+
+// QUESTION#5
+// var tableNumber = 4;
+// document.write ("4x"  + 1 + "=" + tableNumber + "<br>")
+// document.write ("4x"  + 2 + "=" + tableNumber * 2 + "<br>")
+// document.write ("4x"  + 3 + "=" + tableNumber * 3 + "<br>")
+// document.write ("4x"  + 4 + "=" + tableNumber * 4 + "<br>")
+// document.write ("4x"  + 5 + "=" + tableNumber * 5 + "<br>")
+// document.write ("4x"  + 6 + "=" + tableNumber * 6 + "<br>")
+// document.write ("4x"  + 7 + "=" + tableNumber * 7 + "<br>")
+// document.write ("4x"  + 8 + "=" + tableNumber * 8 + "<br>")
+// document.write ("4x"  + 9 + "=" + tableNumber * 9 + "<br>")
+// document.write ("4x"  + 10 + "=" + tableNumber * 10 + "<br>")
+
+// QUESTION#6 (a)
+// var celsius = 25;
+
+// QUESTION#6 (b)
+// fahrenheit = (celsius * 9 / 5) + 32;
+// document.write (celsius + "°C is " + fahrenheit + "°F" + "<br>")
+
+// QUESTION#6 (C)
+// var fahrenheit = 70;
+
+// QUESTION#6 (d)
+// celsius = (fahrenheit - 32) * 5 / 9;
+// document.write (fahrenheit + "°F is " + celsius + "°C")
+
+// QUESTION#7
+// var priceOfItem1 = 650;
+// var priceOfItem2 = 100;
+// var quantityOfItem1 = 3;
+// var quantityOfItem2 = 7;
+// var shippingCharges = 100;
+// var totalCost = ( priceOfItem1 * quantityOfItem1 ) + ( priceOfItem2 * quantityOfItem2 ) + shippingCharges;
+// document.write ("Price of item 1 is " + priceOfItem1 + "<br>");
+// document.write ("Quantity of item 1 is " + quantityOfItem1 + "<br>");
+// document.write ("Price of item 2 is " + priceOfItem2 + "<br>");
+// document.write ("Quantity of item 2 is " + quantityOfItem2 + "<br>");
+// document.write ("Shipping Charges " + shippingCharges + "<br><br>");
+// document.write ("Total cost of your order is " + totalCost);
+
+// QUESTION#8
+// var totalMarks = 980;
+// var marksObtained = 804;
+// var percentage = marksObtained / totalMarks * 100;
+// document.write ("Total marks : " + totalMarks + "<br>");
+// document.write ("Marks obtained : " + marksObtained + "<br>");
+// document.write ("Percentage : " + percentage + "%")
+
+// QUESTION#9
+// var dollars = 10;
+// var riyals = 25;
+// var dollarToPkr = dollars * 104.80;
+// var riyalToPkr = riyals * 28;
+// var totalPkr = dollarToPkr + riyalToPkr;
+// document.write ("Total Curency in PKR : " + totalPkr);
+
+// QUESTION#10
+// var num = 5;
+// num = (num + 5) * 10 /2;
+
+// QUESTION#11 (a)
+// var currentYear = 2016;
+
+// QUESTION#11 (b)
+// var birthYear = 1992;
+
+// QUESTION#11 (c)
+// var age = currentYear - birthYear;
+// document.write ("Current Year : " + currentYear + "<br>");
+// document.write ("Birth Year : " + birthYear + "<br>");
+// document.write ("Your Age is : " + age);
+
+// QUESTION#12 (a)
+// var radius = 20;
+// document.write ("Radius of a circle : " + radius + "<br>");
+
+// QUESTION#12 (b)
+// var circumference = 2 * 3.142 * radius;
+// document.write ("The circumference is : " + circumference + "<br>");
+// var area = 3.142 * (radius * 2);
+// document.write ("The area is : " + area);
+
+// QUESTION#13 (a)
+// var snack = "chocolate chip"
+// document.write ("Favourite Snack : " + snack + "<br>");
+
+// QUESTION#13 (b)
+// var age = 15;
+// document.write ("Current age : " + age + "<br>");
+
+// QUESTION#13 (c)
+// var maximumAge = 65;
+// document.write ("Estimated Maximum Age : " + maximumAge + "<br>");
+
+// QUESTION#13 (c)
+// var amountPerDay = 3;
+// document.write ("Amount of snacks per day : " + amountPerDay + "<br>");
+
+// QUESTION#13 (e)
+// var lifetimeSupply = (maximumAge - age) * 3;
+// document.write ("You will need " + lifetimeSupply+ " " + snack + " to last you until the ripe old age of " + maximumAge);
+
+// ***END*** //
+
+//-----------------------------------------------------------------------------------------//
+
+// ***CHAPTER 6 - 9 : MATH EXPRESSIONS - PROMPTS*** //
+
+// QUESTION#1
+// var a = 10;
+// document.write ("The value of a is : " + a + "<br> -------------------------- <br>")
+// document.write ("The value of ++a is : " + ++a + "<br>")
+// document.write ("Now the value of a is : " + a + "<br><br>")
+// document.write ("The value of a++ is : " + a++ + "<br>")
+// document.write ("Now the value of a is : " + a + "<br><br>")
+// document.write ("The value of --a is : " + --a + "<br>")
+// document.write ("Now the value of a is : " + a + "<br><br>")
+// document.write ("The value of --a is : " + a-- + "<br>")
+// document.write ("Now the value of a is : " + a + "<br><br>")
+
+// QUESTIO#2
+// var a = 2;
+// document.write ("a is " + a + "<br>");
+// var b = 1;
+// document.write ("b is " + b + "<br>");
+// var result = --a - --b + ++b + b--;
+// document.write ("result is " + result + "<br><br>");
+// document.write ("Explanation :" + "<br>");
+// document.write ("--a; = (1)" + "<br>");
+// document.write ("--a - --b; = (1 - 0) = (1)" + "<br>");
+// document.write ("--a - --b + ++b; = (1 - 0 + 1) = (2)" + "<br>");
+// document.write ("--a - --b + ++b + b--; = (1 - 0 + 1 + 1) = (3) answer" + "<br>");
+
+// QUESTION #3
+// var userName = prompt ("Enter your name :");
+// alert ("Hello ! " + userName);
+
+// QUESTION #4,5
+// var tableNumber = +prompt ("Enter a number" , "5");
+// document.write (tableNumber + "x"  + 1 + "=" + tableNumber + "<br>")
+// document.write (tableNumber + "x"  + 2 + "=" + tableNumber * 2 + "<br>")
+// document.write (tableNumber + "x"  + 3 + "=" + tableNumber * 3 + "<br>")
+// document.write (tableNumber + "x"  + 4 + "=" + tableNumber * 4 + "<br>")
+// document.write (tableNumber + "x"  + 5 + "=" + tableNumber * 5 + "<br>")
+// document.write (tableNumber + "x"  + 6 + "=" + tableNumber * 6 + "<br>")
+// document.write (tableNumber + "x"  + 7 + "=" + tableNumber * 7 + "<br>")
+// document.write (tableNumber + "x"  + 8 + "=" + tableNumber * 8 + "<br>")
+// document.write (tableNumber + "x"  + 9 + "=" + tableNumber * 9 + "<br>")
+// document.write (tableNumber + "x"  + 10 + "=" + tableNumber * 10 + "<br>")
+
+// QUESTIO#6 (a)
+// var firstSubject = prompt ("Enter the name of first subject :");
+// var secondSubject = prompt ("Enter the name of second subject :");
+// var thirdSubject = prompt ("Enter the name of third subject :");
+
+// QUESTION#6 (b)
+// var totalMarks = 100;
+
+// QUESTION#6 (c)
+// var marksObtained1stSubject = +prompt ("Enter the marks obtained in first subject : ")
+
+// QUESTION#6 (d)
+// var marksObtained2ndSubject = +prompt ("Enter the marks obtained in second subject : ")
+// var marksObtained3rdSubject = +prompt ("Enter the marks obtained in third subject : ")
+
+// QUESTION #6 (e)
+// var percentageIn1stSubject = marksObtained1stSubject / totalMarks * 100;
+// var percentageIn2ndSubject = marksObtained2ndSubject / totalMarks * 100;
+// var percentageIn3rdSubject = marksObtained3rdSubject / totalMarks * 100;
+// document.write ("<table>");
+// document.write ("<tr>");
+// document.write ("<th>Subjects</th>");
+// document.write ("<th>Total Marks</th>");
+// document.write ("<th>Obtained Marks</th>");
+// document.write ("<th>Percentage</th>");
+// document.write ("</tr>");
+// document.write ("<tr>");
+// document.write ("<td>" + firstSubject + "</td>");
+// document.write ("<td>" + totalMarks + "</td>");
+// document.write ("<td>" + marksObtained1stSubject + "</td>");
+// document.write ("<td>" + percentageIn1stSubject + "</td>");
+// document.write ("</tr>");
+// document.write ("<tr>");
+// document.write ("<td>" + secondSubject + "</td>");
+// document.write ("<td>" + totalMarks + "</td>");
+// document.write ("<td>" + marksObtained2ndSubject + "</td>");
+// document.write ("<td>" + percentageIn2ndSubject + "</td>");
+// document.write ("</tr>");
+// document.write ("<tr>");
+// document.write ("<td>" + thirdSubject + "</td>");
+// document.write ("<td>" + totalMarks + "</td>");
+// document.write ("<td>" + marksObtained3rdSubject + "</td>");
+// document.write ("<td>" + percentageIn3rdSubject + "</td>");
+// document.write ("</tr>");
